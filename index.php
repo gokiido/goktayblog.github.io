8e3854019ab74c8712679fb17e7df9ea057c128e
gokiido <66062285+gokiido@users.noreply.github.com>
2020-07-02 20:57:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,8 @@
 <form action="index.php" method="get">
 <input type="text" name="name">
 <input type="submit">
-<?php 
-Your name is echo $_GET["name"]
 
-?>
+Your name is echo <?php $_GET["name"]; ?>
 
 
 </body>
