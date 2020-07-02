dbeebeb9330a4fc78ab80fe43fceae61faf9a9c2
gokiido <66062285+gokiido@users.noreply.github.com>
2020-07-02 20:51:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<title> goktays blog</title>
+
+
+</head>
+<body>
+<form action="index.php" method="get">
+<input type="text" name="name">
+<input type="submit">
+<?php 
+Your name is echo $_GET["name"]
+
+?>
+
+
+</body>
+
+
+</html>
